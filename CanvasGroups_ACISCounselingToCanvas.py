46d83ea958d66918f3712575fa90468667c827d2
Eric Dannewitz <ericdannewitz@me.com>
2022-01-18 20:03:14 -0800

    Update CanvasGroups_ACISCounselingToCanvas.py

--- CanvasGroups_ACISCounselingToCanvas.py
+++ CanvasGroups_ACISCounselingToCanvas.py
@@ -20,30 +20,26 @@ counselors = [ ('acis','feinberg',10831)]
 canvas = Canvas(Canvas_API_URL,Canvas_API_KEY)
 account = canvas.get_account(1)
 group = canvas.get_group(10831,include=['users'])
-groupusers = pd.DataFrame(group.users,columns=['id','name','login_id'])
-print(groupusers)
+dataframe2 = pd.DataFrame(group.users,columns=['id','name','login_id'])
+print(dataframe2)
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=SATURN;'
                       'Database=DST21000AUHSD;'
                       'Trusted_Connection=yes;')
 cursor = conn.cursor()
 print('All Students for Counselor')
-sql_query = pd.read_sql_query('SELECT ALTSCH.ALTSC, STU.LN, STU.SEM, STU.GR, STU.CU, TCH.EM FROM STU INNER JOIN TCH ON STU.SC = TCH.SC AND STU.CU = TCH.TN INNER JOIN ALTSCH ON STU.SC = ALTSCH.SCID WHERE (STU.SC = 6) AND STU.DEL = 0 AND STU.TG = \'\' AND STU.CU > 0 ORDER BY ALTSCH.ALTSC, STU.CU, STU.LN',conn)
+dataframe1 = pd.read_sql_query('SELECT ALTSCH.ALTSC, STU.LN, STU.SEM, STU.GR, STU.CU, TCH.EM FROM STU INNER JOIN TCH ON STU.SC = TCH.SC AND STU.CU = TCH.TN INNER JOIN ALTSCH ON STU.SC = ALTSCH.SCID WHERE (STU.SC = 6) AND STU.DEL = 0 AND STU.TG = \'\' AND STU.CU > 0 ORDER BY ALTSCH.ALTSC, STU.CU, STU.LN',conn)
 #for EM, SEM in sql_query.groupby('EM'):
 #    print(SEM)
-sql_query.to_csv('AllAeries.csv')
-groupusers.to_csv('AllCanvas.csv')
+dataframe1.to_csv('AllAeries.csv')
+dataframe2.to_csv('AllCanvas.csv')
 
-common = sql_query.merge(groupusers,left_on='SEM',right_on='login_id', how='inner')
+common = dataframe1.merge(dataframe2,left_on='SEM',right_on='login_id', how='inner')
 print('Difference---------')
 print(common)
-tempp = groupusers
-common3 = tempp.merge(sql_query,left_on='login_id',right_on='SEM',how='inner')
-print('Reversed----')
-print(common3)
-sql_query[~(sql_query['SEM'].isin(groupusers['login_id']))]
+dataframe1[~(dataframe1['SEM'].isin(dataframe2['login_id']))]
 print('New diff')
-print(sql_query)
+print(dataframe1)
 #sql_query[(~sql_query.SEM.isin(common.SEM))&(sql_query.login_id.isin(common.login_id))]
 #print(sql_query)
 conn2 = pyodbc.connect('Driver={SQL Server};'
