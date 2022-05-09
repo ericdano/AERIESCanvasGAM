1a099ac5c1d93d3b5f95cd2351c6657aa414bd34
Eric Dannewitz <ericdannewitz@me.com>
2022-05-09 15:42:42 -0700

    Update RemoveSuspended.py

--- RemoveSuspended.py
+++ RemoveSuspended.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import os, sys, pyodbc, shlex, subprocess, gam, datetime, json, smtplib, logging
 
+
+
 #/bin/sh
 #Shell script remove suspended users from groups
-alias gam="/usr/local/gamadv-xtd3/gam"
-/usr/local/gamadv-xtd3/gam print users query isSuspended=true | /usr/local/gamadv-xtd3/gam csv - gam user ~primaryEmail dele
-te groups
+#alias gam="/usr/local/gamadv-xtd3/gam"
+#/usr/local/gamadv-xtd3/gam print users query isSuspended=true | /usr/local/gamadv-xtd3/gam csv - gam user ~primaryEmail delete groups
+
+if __name__ == '__main__':
+    confighome = Path.home() / ".Acalanes" / "Acalanes.json"
