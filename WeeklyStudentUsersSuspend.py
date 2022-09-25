6ae7752959fafc928a28ad2f94651e3aff039d0d
Eric Dannewitz <ericdannewitz@me.com>
2022-09-24 22:39:59 -0700

    Changes - in name and function

--- WeeklyStudentUsersSuspend.py
+++ WeeklyStudentUsersSuspend.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import os, sys, pyodbc, shlex, gam, subprocess, json, logging, smtplib, datetime
+import os, sys, shlex, gam, subprocess, json, logging, smtplib, datetime
 from pathlib import Path
-from canvasapi import Canvas
-from canvasapi.exceptions import CanvasException
 from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -22,7 +20,7 @@ def main()
   msg['From'] = configs['SMTPAddressFrom']
   msg['To'] = configs['SendInfoEmailAddr']
   msgbody = ''
-  thelogger.info('WeeklyStudentSuspend_>Starting GAM Suspension')
+  thelogger.info('WeeklyStudentSuspend->Starting GAM Suspension')
   stat = gam.CallGAMCommand(['gam','ou_and_children','"/Z-Former Students"','update','user','suspended','true'])
   if stat != 0:
     msg['Subject'] = "ERROR! " + str(configs['SMTPStatusMessage'] + " Weekly Student Suspension Script " + datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"))
@@ -33,8 +31,8 @@ def main()
   msg.set_content(msgbody)
   s = smtplib.SMTP(configs['SMTPServerAddress'])
   s.send_message(msg)
-  thelogger.info('UpdateCounselingListsInGoogle->Sent status message')
-  thelogger.info('UpdateCounselingListsInGoogle->DONE! - took ' + str(end_of_timer - start_of_timer))
+  thelogger.info('WeeklyStudentSuspend->Sent status message')
+  thelogger.info('WeeklyStudentSuspend->DONE! - took ' + str(end_of_timer - start_of_timer))
   print('Done!!!')
   
 if __name__ == '__main__':
