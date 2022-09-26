2f858c65b6946b2ca821041654f3fdbc3ae3fff4
Eric Dannewitz <ericdannewitz@me.com>
2022-09-26 09:36:18 -0700

    Update EnrollUserIntoSectionTest.py

--- EnrollUserIntoSectionTest.py
+++ EnrollUserIntoSectionTest.py
@@ -24,16 +24,17 @@ course.enroll_user(
 course.enroll_user(
     user,
     enrollment_type = "StudentEnrollment",
-    enrollment={
-        'course_section_id': 10037,
-        'enrollment_state': 'active'
-    }
+    enrollment={'enrollment_state': 'active'}
 )
-course.enroll_user(
-    user,
-    enrollment_type = "StudentEnrollment",
-    enrollment={
-        'course_section_id': 10038,
-        'enrollment_state': 'active'
-    }
-)
+enrollments = course.get_enrollments(type='StudentEnrollment')
+for stu in enrollments:
+    if stu.user_id == user.id:
+        print('Found ->' + str(stu.id) + ' ' + str(stu.type) + ' ' + str(stu.user_id) + ' ' + str(stu.course_section_id))
+        print(vars(stu))
+#            lookfordelete = False
+#            for stu in enrollments:
+#                # You have to loop through all the enrollments for the class and then find the student id in the enrollment then tell it to delete it.
+#                if stu.user_id == user.id:
+#                    lookfordelete = True
+#                    stu.deactivate(task='delete')
+print('Done')
