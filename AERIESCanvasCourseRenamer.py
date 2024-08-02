1655a8abe29742c390d10b18218ac21a11c6e1e9
Eric Dannewitz <ericdannewitz@me.com>
2024-08-02 08:42:29 -0700

    Changes

--- AERIESCanvasCourseRenamer.py
+++ AERIESCanvasCourseRenamer.py
@@ -42,9 +42,11 @@ if __name__ == '__main__':
     msg['From'] = configs['SMTPAddressFrom']
     msg['To'] = configs['SendInfoEmailAddr']
     msgbody = ''
-    msgbody += 'Using Database->' + str(configs['AERIESSQLServerSandbox']) + '\n'
+    msgbody += 'Using Database->' + str(configs['AERIESSQLServer']) + '\n'
     WasThereAnError = False
-    connection_string = "DRIVER={SQL Server};SERVER=" + configs['AERIESSQLServerSandbox'] + ";DATABASE=" + configs['AERIESDatabaseSB'] + ";UID=" + configs['AERIESUsername'] + ";PWD=" + configs['AERIESPassword'] + ";"
+#    connection_string = "DRIVER={SQL Server};SERVER=" + configs['AERIESSQLServerSandbox'] + ";DATABASE=" + configs['AERIESDatabaseSB'] + ";UID=" + configs['AERIESUsername'] + ";PWD=" + configs['AERIESPassword'] + ";"
+    connection_string = "DRIVER={SQL Server};SERVER=" + configs['AERIESSQLServer'] + ";DATABASE=" + configs['AERIESDatabase'] + ";UID=" + configs['AERIESUsername'] + ";PWD=" + configs['AERIESPassword'] + ";"
+
     connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
     engine = create_engine(connection_url)
     
@@ -52,20 +54,24 @@ if __name__ == '__main__':
     #sql_query = pd.read_sql_query("""SELECT CRS.CN AS CourseID, CRS.CO AS CourseTitle, MST.SC AS School, MST.SE AS SectionNum, MST.SE AS ShortTitle, FTF.STI AS CourseNum, MST.CN AS CourseName, STF.LN AS LastName FROM MST INNER JOIN SSE ON MST.SC = SSE.SC AND MST.SE = SSE.SE INNER JOIN FTF ON MST.FSQ = FTF.SQ INNER JOIN STF ON SSE.ID = STF.ID INNER JOIN CRS ON MST.CN = CRS.CN WHERE MST.DEL = 0 AND (MST.CN <> 'OS535E' AND MST.CN <> 'PREPTO' AND MST.CN <> 'O0535E' AND MST.CN <> 'O0544E') ORDER BY SCHOOL, LASTNAME, COURSENAME, SECTIONNUM""", engine)
     #
     #LLHS Query
-    sql_query = pd.read_sql_query("""SELECT CRS.CN AS CourseID, CRS.CO AS CourseTitle, MST.SC AS School, MST.SE AS SectionNum, MST.SE AS ShortTitle, FTF.STI AS CourseNum, MST.CN AS CourseName, STF.LN AS LastName FROM MST INNER JOIN SSE ON MST.SC = SSE.SC AND MST.SE = SSE.SE INNER JOIN FTF ON MST.FSQ = FTF.SQ INNER JOIN STF ON SSE.ID = STF.ID INNER JOIN CRS ON MST.CN = CRS.CN WHERE MST.SC = 4 AND MST.DEL = 0 AND (MST.CN <> 'OS535E' AND MST.CN <> 'PREPTO' AND MST.CN <> 'O0535E' AND MST.CN <> 'O0544E' AND MST.CN <> 'Z5011N' AND MST.CN <> 'Z5016N' AND MST.CN <> 'Z5017N') ORDER BY SCHOOL, LASTNAME, COURSENAME, SECTIONNUM""",engine)
+    #sql_query = pd.read_sql_query("""SELECT CRS.CN AS CourseID, CRS.CO AS CourseTitle, MST.SC AS School, MST.SE AS SectionNum, MST.SE AS ShortTitle, FTF.STI AS CourseNum, MST.CN AS CourseName, STF.LN AS LastName FROM MST INNER JOIN SSE ON MST.SC = SSE.SC AND MST.SE = SSE.SE INNER JOIN FTF ON MST.FSQ = FTF.SQ INNER JOIN STF ON SSE.ID = STF.ID INNER JOIN CRS ON MST.CN = CRS.CN WHERE MST.SC IN (1,2,3,4,6,7) AND MST.DEL = 0 AND (MST.CN <> 'OS535E' AND MST.CN <> 'PREPTO' AND MST.CN <> 'O0535E' AND MST.CN <> 'O0544E' AND MST.CN <> 'Z5011N' AND MST.CN <> 'Z5016N' AND MST.CN <> 'Z5017N') ORDER BY SCHOOL, LASTNAME, COURSENAME, SECTIONNUM""",engine)
+    sql_query = pd.read_sql_query("""SELECT CRS.CN AS CourseID, CRS.CO AS CourseTitle, MST.SC AS School, MST.SE AS SectionNum, MST.SE AS ShortTitle, FTF.STI AS CourseNum, MST.CN AS CourseName, STF.LN AS LastName FROM MST INNER JOIN SSE ON MST.SC = SSE.SC AND MST.SE = SSE.SE INNER JOIN FTF ON MST.FSQ = FTF.SQ INNER JOIN STF ON SSE.ID = STF.ID INNER JOIN CRS ON MST.CN = CRS.CN WHERE MST.SC IN (6) AND MST.DEL = 0 AND (MST.CN <> 'OS535E' AND MST.CN <> 'PREPTO' AND MST.CN <> 'O0535E' AND MST.CN <> 'O0544E' AND MST.CN <> 'Z5011N' AND MST.CN <> 'Z5016N' AND MST.CN <> 'Z5017N') ORDER BY SCHOOL, LASTNAME, COURSENAME, SECTIONNUM""",engine)
 
     #print(sql_query)
     sql_query["SIS_ID"] = "2025~" + sql_query["School"].astype(str) + "_" + sql_query["SectionNum"].astype(str)
-    sql_query["NewCourseTitle"] = "24-25 " + sql_query["CourseTitle"].astype(str) + " - " + sql_query["LastName"] + " " + sql_query['CourseNum']
+    #sql_query["NewCourseTitle"] = "24-25 " + sql_query["CourseTitle"].astype(str) + " - " + sql_query["LastName"] + " " + sql_query['CourseNum']
+    sql_query["NewCourseTitle"] = "24-25 " + sql_query["CourseTitle"].astype(str) + " - " + sql_query["LastName"] 
     sql_query["NewCourseTitleSort"] = "24-25 " + sql_query["CourseTitle"].astype(str) + " - " + sql_query["LastName"]
-    #sql_query.to_csv('export.csv')
+    sql_query["NewCourseSectionData"] = sql_query["SectionNum"].astype(str) + " - " + sql_query["CourseTitle"].astype(str)
+    sql_query.to_csv('export.csv')
     #print(sql_query)
 
     #-----Canvas Info
     #sql_query.to_csv('export2.csv')
     
     # Using BETA URL 
-    Canvas_API_URL = configs['CanvasBETAAPIURL']
+    # Canvas_API_URL = configs['CanvasBETAAPIURL']
+    Canvas_API_URL = configs['CanvasAPIURL']
     Canvas_API_KEY = configs['CanvasAPIKey']
     thelogger.info('AERIES Canvas Course Renamer->Connecting to Canvas')
     canvas = Canvas(Canvas_API_URL,Canvas_API_KEY)
@@ -93,22 +99,7 @@ if __name__ == '__main__':
                 print("created course " + str(sql_query['SIS_ID'][i]) + " in Canvas")
     exit(1)
     """
-    #------------------------------------------------------
-    # Rename Courses
-    # This takes the inital AERIES data and renames classes
-    #----------------------
-    """
-    for i in sql_query.index:
-        try:
-            CourseToRename = canvas.get_course(sql_query['SIS_ID'][i],use_sis_id=True)
-            print("Renaming course " + str(sql_query['SIS_ID'][i]) + " to " + str(sql_query['NewCourseTitle'][i]))
-            CourseToRename.update(course={'course_code': sql_query['NewCourseTitle'][i],
-                                          'name': sql_query['NewCourseTitle'][i]})
-        except CanvasException as e:
-            if str(e) == "Not Found":
-                print("PANIC - Course SIS_ID " + str(sql_query['SIS_ID'][i]) + " seems not to be in Canvas but is in AERIES")
-    exit(1)
-    """
+
     # Reset Sample Data
     # Resets Renamed data back to what it was when AERIES brought it over
     #
@@ -124,17 +115,14 @@ if __name__ == '__main__':
                 print("PANIC - Course SIS_ID " + str(sql_query['SIS_ID'][i]) + " seems not to be in Canvas but is in AERIES")       
     """
     # Courses generated by AERIES into Canvas have a Section with them. We need to create the sections for sample data
-    # 
+    # SAMPLE DATA STUFF - In production we should NOT be messing with Section names and stuff
     """
     for i in sql_query.index:
         print('Adding Section to ' + str(sql_query['SIS_ID'][i]) )
         course = canvas.get_course(sql_query['SIS_ID'][i],use_sis_id=True)
-        course.create_course_section(course_section={"name":sql_query['NewCourseTitle'][i],
+        course.create_course_section(course_section={"name":sql_query['NewCourseSectionData'][i],
                                                      "sis_section_id":sql_query['SIS_ID'][i]})
     exit(1)
-    """
-
-    """
     ---------------------------------------------------------
     # Fix for not having SIS_ID in the section
     # Don't need to run, saved for historical reference
@@ -155,14 +143,12 @@ if __name__ == '__main__':
     print(bool_series)
     sql_query['Dup'] = bool_series
     print(sql_query)
-    #sql_query.to_csv('export3.csv')
-    CurrentMasterSectionSIS_ID = 0
-    CurrentMasterSectionCourseName = ""
+    sql_query.to_csv('exportbool.csv')
+    """
     # -----------------------
     # This just sees what sections are related in each course
     # each course has to have a section
     #
-    """
     for i in sql_query.index:
         course = canvas.get_course(sql_query['SIS_ID'][i],use_sis_id=True)
         sections = course.get_sections()
@@ -171,24 +157,77 @@ if __name__ == '__main__':
             print(section.name +" id - " + str(section.id))
 
     """
-    # -----------------------
+    #------------------------------------------------------
+    # Rename Courses
+    # This takes the inital AERIES data and renames classes
+    #----------------------
+    for i in sql_query.index:
+        try:
+            CourseToRename = canvas.get_course(sql_query['SIS_ID'][i],use_sis_id=True)
+            print("Renaming course " + str(sql_query['SIS_ID'][i]) + " to " + str(sql_query['NewCourseTitle'][i]))
+            CourseToRename.update(course={'course_code': sql_query['NewCourseTitle'][i],
+                                          'name': sql_query['NewCourseTitle'][i]})
+        except CanvasException as e:
+            if str(e) == "Not Found":
+                print("PANIC - Course SIS_ID " + str(sql_query['SIS_ID'][i]) + " seems not to be in Canvas but is in AERIES")
+                msgbody += "PANIC - Course SIS_ID " + str(sql_query['SIS_ID'][i]) + " seems not to be in Canvas but is in AERIES\n"
+   
+    exit(1)
     
+    CurrentMasterSectionSIS_ID = 0
+    CurrentMasterSectionCourseName = ""
+
+
+    # -----------------------
+    # Cross listing
     for i in sql_query.index:
         if sql_query['Dup'][i] == False:
-            CurrentMasterSectionSIS_ID =  canvas.get_course(sql_query['SIS_ID'][i],use_sis_id=True)
-            CurrentMasterSectionCourseName = sql_query['NewCourseTitle'][i]
-            print('Found a potential class->' + str(sql_query['SIS_ID'][i]))
-            msgbody += ('Found a potential class->' + str(sql_query['SIS_ID'][i]) + ' ' + sql_query['NewCourseTitle'][i] + '\n')
+            try:
+                CurrentMasterSectionSIS_ID =  canvas.get_course(sql_query['SIS_ID'][i],use_sis_id=True)
+                CurrentMasterSectionCourseName = sql_query['NewCourseTitle'][i]
+                CurrentMasterSelectionSimpleCourseName = sql_query['NewCourseTitleSort'][i]
+                print('Found a potential class->' + str(sql_query['SIS_ID'][i]))
+                msgbody += ('Found a potential class->' + str(sql_query['SIS_ID'][i]) + ' ' + sql_query['NewCourseTitle'][i] + '\n')
+            except CanvasException as f:
+                if str(f) == "Not Found":
+                    print("Panic - Look up of Course SIS_ID " + str(sql_query['SIS_ID'][i]) + " does not seem to be in Canvas but is in AERIES")
+                    msgbody += "Panic - Look up of Course SIS_ID " + str(sql_query['SIS_ID'][i]) + " does not seem to be in Canvas but is in AERIES\n"
         elif sql_query['Dup'][i] == True:
-            course = canvas.get_course(sql_query['SIS_ID'][i],use_sis_id=True)
-            sections = course.get_sections()
-            for section in sections:
-                new_section = section.cross_list_section(CurrentMasterSectionSIS_ID)
-            print('Cross listed ' + str(sql_query['SIS_ID'][i]) + ' to ' + str(CurrentMasterSectionSIS_ID) + ' ' + CurrentMasterSectionCourseName)
-            msgbody += 'Crosslisted ->' + str(sql_query['SIS_ID'][i]) + ' ' + sql_query['NewCourseTitle'][i] + ' to SIS_ID->' + str(CurrentMasterSectionSIS_ID) + ' ' + CurrentMasterSectionCourseName + '\n'
+            # Here, we seem to have a course that should be cross listed
+            # we have the SIS_ID of the FIRST Canvas course, and we need to rename it, getting rid of the P2 or whatever after it
+            try:
+                course = canvas.get_course(sql_query['SIS_ID'][i],use_sis_id=True)
+                sections = course.get_sections()
+                #cross list the section
+                for section in sections:
+                    new_section = section.cross_list_section(CurrentMasterSectionSIS_ID)
+                print('Cross listed ' + str(sql_query['SIS_ID'][i]) + ' to ' + str(CurrentMasterSectionSIS_ID) + ' ' + CurrentMasterSectionCourseName)
+                msgbody += 'Crosslisted ->' + str(sql_query['SIS_ID'][i]) + ' ' + sql_query['NewCourseTitle'][i] + ' to SIS_ID->' + str(CurrentMasterSectionSIS_ID) + ' ' + CurrentMasterSectionCourseName + '\n'
+                # Rename the course title of the section we are cross listing
+                try:
+                    course.update(course={'course_code': CurrentMasterSelectionSimpleCourseName,
+                                            'name': CurrentMasterSelectionSimpleCourseName})
+                except CanvasException as q:
+                    if str(q) == "Not Found":
+                        print("PANIC - Course SIS_ID " + str(sql_query['SIS_ID'][i]) + " seems not to be in Canvas but is in AERIES")
+                        msgbody += "PANIC - Course SIS_ID " + str(sql_query['SIS_ID'][i])+ " seems not to be in Canvas but is in AERIES\n"
+                #Rename the 'Master Class'
+                try:
+                    CourseToRename = canvas.get_course(CurrentMasterSectionSIS_ID)
+                    CourseToRename.update(course={'course_code': CurrentMasterSelectionSimpleCourseName,
+                                            'name': CurrentMasterSelectionSimpleCourseName})
+                except CanvasException as e:
+                    if str(e) == "Not Found":
+                        print("PANIC - Course SIS_ID " + str(CurrentMasterSectionSIS_ID) + " seems not to be in Canvas but is in AERIES")
+                        msgbody += "PANIC - Course SIS_ID " + str(CurrentMasterSectionSIS_ID) + " seems not to be in Canvas but is in AERIES\n"
+            except CanvasException as g:
+                if str(g) == "Not Found":
+                    print("PANIC - Potential course for cross reference, Course SIS_ID " + str(CurrentMasterSectionSIS_ID) + " seems not to be in Canvas but is in AERIES")
+                    msgbody += "PANIC - Potential course for cross reference, Course SIS_ID " + str(CurrentMasterSectionSIS_ID) + " seems not to be in Canvas but is in AERIES\n"
         else:
             print('Error!')
             exit(1)
+
     print("Done!")
     msg['Subject'] = str(configs['SMTPStatusMessage'] + " Canvas Renamer and Crosslister " + datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"))
     end_of_timer = timer()
--- Aeries2FAResetManual.py
+++ Aeries2FAResetManual.py
@@ -54,7 +54,7 @@ mfa2reset = 'auhsd\edannewitz'
 
 engine = create_engine(connection_url)
 with engine.begin() as connection:
-    result = connection.execute(text("""UPDATE UGN SET MFA =0 WHERE UN ='staff\jmerken'"""))
+    result = connection.execute(text("""UPDATE UGN SET MFA =0 WHERE UN ='staff\xmartinez'"""))
 print(result)
 if WasThereAnError:
     msg['Subject'] = "ERROR! " + str(configs['SMTPStatusMessage'] + " AERIES 2FA Reset " + datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"))
