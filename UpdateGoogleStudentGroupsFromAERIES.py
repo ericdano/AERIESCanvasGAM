d5907beb8bc667cccab63aacd952a5f72d1a2c81
Eric Dannewitz <ericdannewitz@me.com>
2025-08-16 09:52:45 -0700

    Update UpdateGoogleStudentGroupsFromAERIES.py

--- UpdateGoogleStudentGroupsFromAERIES.py
+++ UpdateGoogleStudentGroupsFromAERIES.py
@@ -109,8 +109,8 @@ print(file_list)
 # and now we use that to call GAM to update the list from the CSV
 for row in file_list.itertuples(index=False):
     print(f"filename: {row.filename}, groupname: {row.groupname}")
-    thelogger.info("Student Google Group Updater>Processing filename: {row.filename}, groupname: {row.groupname}")
-    msgbody += "Processing filename: {row.filename}, groupname: {row.groupname}\n"
+    thelogger.info(f"Student Google Group Updater>Processing filename: {row.filename}, groupname: {row.groupname}")
+    msgbody += f"Processing filename: {row.filename}, groupname: {row.groupname}\n"
     #stat1 = gam.CallGAMCommand(['gam','update', 'group', '{row.group_name}', 'sync', 'members', 'file', '{row.file_name}'])
     #if stat1 != 0:
         #WasThereAnError = True
