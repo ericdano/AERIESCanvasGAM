1a6cf7ff28d222d850a71d639fd4697e437ba6ff
Eric Dannewitz <ericdannewitz@me.com>
2026-01-14 11:36:07 -0800

    Update AERIEStoGoogleSheetTest.py

--- AERIEStoGoogleSheetTest.py
+++ AERIEStoGoogleSheetTest.py
@@ -13,26 +13,12 @@ from email.mime.image import MIMEImage
 from logging.handlers import SysLogHandler
 
 """
- Python 3.9+ script to pull data from AERIES and to send it to ASB Works.
+ Python 3.9+ script to pull data from AERIES and to create and send it to a Google Shee
  Built in support for this is busted in AERIES as of 5/2022
  Uses a .JSON file specified in confighome which has a logserveraddress, and the login info for ASB Works.
 """
 
 
-class ReusedSslSocket(SSLSocket):
-    def unwrap(self):
-        pass
-
-class MyFTP_TLS(ftplib.FTP_TLS):
-    #Explicit FTPS, with shared TLS session
-    def ntransfercmd(self, cmd, rest=None):
-        conn, size = ftplib.FTP.ntransfercmd(self, cmd, rest)
-        if self._prot_p:
-            conn = self.context.wrap_socket(conn,
-                                            server_hostname=self.host,
-                                            session=self.sock.session)  # reuses TLS session            
-            conn.__class__ = ReusedSslSocket  # we should not close reused ssl socket when file transfers finish
-        return conn, size
 
 if __name__ == '__main__':
     start_of_timer = timer()
