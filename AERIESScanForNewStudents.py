06b98ef340940edc435f4a36544a694689e59e39
Eric Dannewitz <ericdannewitz@me.com>
2026-04-03 19:58:38 -0700

    Update AERIESScanForNewStudents.py

--- AERIESScanForNewStudents.py
+++ AERIESScanForNewStudents.py
@@ -1,4 +1,4 @@
-import io, ftplib, ssl, sys, os, datetime, json, smtplib, logging
+import io, ftplib, ssl, sys, os, datetime, json, smtplib, logging, requests, socket
 from sqlalchemy.engine import URL
 from sqlalchemy import create_engine
 from ldap3 import Server, Connection, ALL
@@ -15,6 +15,47 @@ from logging.handlers import SysLogHandler
 """
  Python 3.12+
 """
+class GraylogGELFHTTPHandler(logging.Handler):
+    """Custom logging handler to send GELF-formatted logs to Graylog over HTTPS."""
+    
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+        self.hostname = socket.gethostname()
+
+    def emit(self, record):
+        try:
+            # Map Python logging levels to standard Syslog levels required by GELF
+            level_mapping = {
+                logging.DEBUG: 7,
+                logging.INFO: 6,
+                logging.WARNING: 4,
+                logging.ERROR: 3,
+                logging.CRITICAL: 2
+            }
+            
+            # Construct the GELF JSON payload
+            payload = {
+                "version": "1.1",
+                "host": self.hostname,
+                "short_message": self.format(record),
+                "timestamp": record.created,
+                "level": level_mapping.get(record.levelno, 6),
+                "_logger_name": record.name,
+                "_file": record.pathname,
+                "_line": record.lineno
+            }
+            
+            # Include the exception traceback if an error occurs
+            if record.exc_text:
+                payload["full_message"] = record.exc_text
+                
+            # Fire and forget the POST request
+            requests.post(self.url, json=payload, timeout=5)
+            
+        except Exception:
+            # Standard logging fallback to prevent log failures from crashing your app
+            self.handleError(record)
 
 if __name__ == '__main__':
     start_of_timer = timer()
@@ -23,7 +64,7 @@ if __name__ == '__main__':
         configs = json.load(f)
     thelogger = logging.getLogger('MyLogger')
     thelogger.setLevel(logging.DEBUG)
-    handler = logging.handlers.SysLogHandler(address = (configs['logserveraddress'],514))
+    handler = GraylogGELFHTTPHandler(configs['logserveraddress'])
     thelogger.addHandler(handler)
     #prep status (msg) email
     s = smtplib.SMTP(configs['SMTPServerAddress'])
@@ -32,7 +73,7 @@ if __name__ == '__main__':
     msg['To'] = 'edannewitz@auhsdschools.org'
     
     WasThereAnError = False
-    thelogger.info('AERIES Scan for New Students->Connecting To AERIESa')
+    thelogger.info('AERIES Scan for New Students->Connecting To AERIES')
     connection_string = "DRIVER={SQL Server};SERVER=" + configs['AERIESSQLServer'] + ";DATABASE=" + configs['AERIESDatabase'] + ";UID=" + configs['AERIESUsername'] + ";PWD=" + configs['AERIESPassword'] + ";"
     connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
     engine = create_engine(connection_url)
@@ -89,10 +130,6 @@ if __name__ == '__main__':
         </body>
         </html>
     """
-    #sql_query.to_csv(dest_filename, index = False)
-    #thelogger.info('AERIES Scan for New Students->Wrote temp CSV to disk')
-    #thelogger.info('AERIES Scan for New Students->Connecting to ASB Works via FTPS')
-    #exit(1)
 
     if WasThereAnError:
         msg['Subject'] = "ERROR! " + str(configs['SMTPStatusMessage'] + " AERIES New Student Scan " + datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"))
@@ -101,3 +138,4 @@ if __name__ == '__main__':
     msg.attach(MIMEText(html_body,'html'))
     s = smtplib.SMTP(configs['SMTPServerAddress'])
     s.send_message(msg)
+    thelogger.info('AERIES Scan for New Students->Sent email with results')
